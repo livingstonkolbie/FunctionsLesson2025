f1f8f69e875b5da9b9ddc7f5638472d9f3357d21
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-05-12 11:59:06 -0600

    Update functions1.js
    need to sit and take the time to get deep understanding of this...

--- js/functions1.js
+++ js/functions1.js
@@ -55,4 +55,81 @@ function mathQuiz() {
     }
 }
 
-mathQuiz();
+// mathQuiz();
+
+
+// Problem 4 Maximum of Two Values
+
+// I - prompt() user to enter 2 values
+// P - maximum() - this compares the two values and selects the bigger one
+// O - display the value that is the greater of the 2
+
+function maximum() {
+    let num1 = Number(prompt('enter the first number'));
+    let num2 = Number(prompt('enter the second number'));
+    
+    if(num1 > num2) {
+        console.log(`${num1} is the greater number`);
+    } else if(num1 < num2) {
+        console.log(`${num2} is the greater number`);
+    } else {
+        console.log('the two numbers you entered are the same');
+    }
+}
+
+// maximum();
+
+
+
+
+// Problem 5 Falling Distance [COULDNT FIGURE OUT]
+
+//I - create a loop that passes values 1 - 10 as arguments
+//P - D - (1/2) * 9.8 * t * t ... fallingDistance(timeInSeconds)  
+//O - return the distance in meters that the object has fallen during that time interval 
+
+// create a program that calls the function in a loop that passes the values 1-10
+
+let arrDistance = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for(let i = 1; i <= 10; i++) {
+
+    function fallingDistance(arrDistance) {
+        let distance = (i*i) * (1/2) * 9.8;
+
+        return arrDistance;
+    }
+
+}
+
+// fallingDistance();
+
+
+
+// Problem 6 Test Avg Grade
+
+// I - user enters 5 test scores
+// P - calcAverage() determineGrade() 
+// O - display a letter grade for each score and the avg test score
+
+let numberOfGrades = 5;
+let grades = [];
+
+function calcAverage() {
+    for (let j = 0; j < numberOfGrades; j++) {
+        let studentGrades = Number(prompt(`enter grade ${j+1}`));
+        grades.push(studentGrades);
+    }
+
+    console.log(`the grades are ${grades}`); // an array of the student's inputs
+
+    let avgGrades = 0;
+    
+    for(let l = 0; l < grades.length; l++) {
+        avgGrades += grades[l] / 5;
+    }
+
+    console.log(`the avg grade is ${avgGrades}`);
+}
+
+calcAverage();
